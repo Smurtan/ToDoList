eba41655a685d770f07a4b6fe5af155281032ff6
Yycheslav <smirn.005vychislavoo@gmail.com>
2022-08-16 09:34:27 +0300

    Added the ability to view the list and delete tasks

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
 class Viewing:
     def __init__(self):
-        self.indent = ' ' * 4
-        with open('todoList.txt', 'r') as file:
-            self.todolist = file.read().split(', ')
+        self.indent = ' ' * 4  # setting up the indentation
+        with open('todoList.txt', 'r') as f:  # opening the file for reading
+            self.todolist = f.read().split(', ')  # reading the contents into the list
 
     def __str__(self):
+        print('--' * 70)  # separator
         print('Your business for today:')
-        for case in self.todolist:
-            print(self.indent + case)
-        print()
+        for index in range(len(self.todolist)):  # through the indexes of the list we output
+            print('%s %i) %s' % (self.indent, index + 1, self.todolist[index]))  # the corresponding task and its number
+        print('--' * 70)  # separator
 
+    def add(self, other):
+        other = other.replace('+', '')  # remove the "+" character
+        if other[0] == ' ':  # removing the initial space
+            other = other[1:]
+        self.todolist.append(other)  # adding a task to the list
+        print('Задача "%s" успешно добавлена.' % other)  # the line about the execution of the action
 
-class Add(Viewing):
-    pass
-
-
-class Delete(Viewing):
-    pass
-
-
-class Rename(Viewing):
-    pass
-
-
-class Do(Viewing):
-    pass
+    def remove(self, index):
+        index = index.replace('-', '').replace(' ', '')  # getting rid of extra characters
+        gap = self.todolist.pop(int(index) - 1)  # memorizing a deleted task
+        print('Задача "%s" успешно удалена.' % gap)  # the line about the execution of the action
 
 
 if __name__ == '__main__':
+    print('Инструкция:\n'
+          'Чтобы добавить задачу в список дел, введите "+" перед названием задачи.\n'
+          'Если вы хотите удалить задачу, введите "-" с указанием номера задачи, от которой необходимо избавиться.\n'
+          'Чтобы увидеть весь список задач введите "w"(watch).\n'
+          'Для выхода введите "e"(exit).')
     user = Viewing()  # A copy of the to-do list
     user.__str__()  # viewing existing tasks
     try:
         while True:  # Program cycle
-            user.line = input(
-                'Если Вы хотите добавить задачу, введите "+" перед ней \n--> '
-            )  # The line in which the user interacts with the program
+            user.line = input('Что Вы хотите сделать? --> ')  # The line in which the user interacts with the program
 
             try:
-                if user.line[0] == '+' or user.line[1] == '+':  # adding a task
-                    user.line = user.line.replace('+', '')  # remove the "+" character
-                    if user.line[0] == ' ':  # removing the initial space
-                        user.line = user.line[1:]
-                    user.todolist.append(user.line)  # adding a task to the list
-                    print('Вы добавили задачу: %s' % user.line)
+                if user.line[0] == '+':  # adding a task
+                    user.add(user.line)
+
+                elif user.line[0] == '-':  # deleting a task
+                    user.remove(user.line)
+
+                elif user.line[0] == 'w':  # view the to-do list
+                    user.__str__()
+
+                elif user.line[0] == 'e':  # exiting the application
+                    user.__str__()
+
+                elif user.line[0] == ' ':  # if there is an extra space at the beginning
+                    print('Пожалуйста, уберите лишний пробел в начале, и попробуйте снова:')
             except IndexError:
-                pass
+                print('Небыло указано никаких действий.\n')
     finally:
-        with open('todoList.txt', 'w') as file:
-            file.write(str(user.todolist)[1:-1].replace("'", ""))
-            user.__str__()
+        with open('todoList.txt', 'w') as file:  # If for some reason the program was closed, then before exiting
+            file.write(str(user.todolist)[1:-1].replace("'", ""))  # the entire file is overwritten with a line
